cfd4207287dcbd823cf5e1fd187a7136be643155
Chirag <chhirag920@gmail.com>
2026-05-23 15:40:46 +0530

    user registration auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const pool = require('../db')
 // POST /register
+// this is the RULEBOOK - defined once outside
+const registrationRules = [
+  body('username').notEmpty(),
+  body('email').isEmail(),
+  body('password').isLength({ min: 6 })
+]
 // 1. get username, email, password from req.body
+router.post('/register', registrationRules, async (req, res) => {
+try{
 const {username, email, password} = req.body 
 // 2. validate - are fields empty? is email valid?
-const { body, validationResult } = require('express-validator');
-// rules for validation
-const validationRules = [
-    body('username').notEmpty().withMessage('Username is required'),
-    body('email').isEmail().withMessage('Invalid email format'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-]
 const errors = validationResult(req);
 if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
 }
 // 3. check if email already exists in DB
+const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+if(userExists.rows.length > 0) {
+    return res.status(409).json({error: "Email already registered"});
+}
 
 // 4. hash the password with bcrypt
+const hashPassword = await bcrypt.hash(password, 12);
 // 5. save user to DB
+ const newUser = await pool.query(
+    'INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *',
+    [username, email, hashPassword]
+  )
+
 // 6. return success
+res.status(201).json({
+    message: "User registered successfully",
+    token: jwt.sign({ id: newUser.rows[0].id, username: newUser.rows[0].username }, process.env.JWT_SECRET, { expiresIn: '1h' }),
+    user: newUser.rows[0],
+})
+} catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Server error' })
+}
+});
 
 // POST /login
 // 1. get email, password from req.body
